2e0925f13e25efe106234431b524bac230162e15
Dan <dtovbein@gmail.com>
2015-03-02 15:23:06 -0300

    Renombre Utilities.js por Monkeyman.js para poder obytener la version. Renombre View por Generic View y el initialize lo hace directamente desde el constructor

--- src/core/snippets/GenericSnippet.js
+++ src/core/snippets/GenericSnippet.js
@@ -1,16 +1,25 @@
 function GenericSnippet(config) {
 	this.config = config;
-	this.container = this.config.container;
+	this.initializeParameters();
+	this.initialize();
 }
 
 GenericSnippet.prototype.constructor = GenericSnippet;
 
-GenericSnippet.prototype.initializeParameters = function() {}
+GenericSnippet.prototype.initializeParameters = function() {
+	this.container = this.config.container;
+}
 
 GenericSnippet.prototype.initialize = function() {
-	var snippet = new Snippet( { path : this.pathSnippet , data : (this.dataSnippet != undefined) ? this.dataSnippet : [] } );
+	//alert("OJO QUE PUEDEN HABER MAS DE DOS initialize en y atencion en el valor pathSnippet ",this);
+	debugger;
+	var snippet = new Snippet( { path : this.path , data : (this.dataSnippet != undefined) ? this.dataSnippet : [] } );
 	this.node = $.parseHTML(snippet.getSnippet());
 	this.container.append(this.node);
 }
 
 GenericSnippet.prototype.addHandlers = function() {}
+
+GenericSnippet.prototype.destroy = function() {
+	$(this.node).remove();
+}
--- src/core/snippets/Popup.js
+++ src/core/snippets/Popup.js
@@ -6,6 +6,10 @@ inheritPrototype(Popup,GenericSnippet);
 
 Popup.prototype.constructor = Popup;
 
+Popup.prototype.initializeParameters = function() {
+	GenericSnippet.prototype.initializeParameters.call(this);
+}
+
 Popup.prototype.addHandlers = function() {
 	GenericSnippet.prototype.addHandlers.call(this);
 }
